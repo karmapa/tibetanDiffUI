01194380e67c491a613cf2411de82ce34c91a18d
zhi-xuan <rickie120243@gmail.com>
2016-04-29 15:27:50 +0800

    add theme select

---
+++ src/containers/changeTheme.js
@@ -0,0 +1,46 @@
+export const changeTheme = (color) => {
+  const themeTypes = {
+    green: {
+      bg: '#EFF2E4',
+      title: '#D3FFBE',
+      logo: 'transparent',
+      main: '#000000',
+      logoTitle: '#4A8731',
+      text: '#FFFFFF'
+    },
+    blue: {
+      bg: '#D7ECFF',
+      title: '#B7F2FF',
+      logo: 'transparent',
+      main: '#000000',
+      logoTitle: '#4A8731',
+      text: '#FFFFFF'
+    },
+    black: {
+      bg: '#000000',
+      title: '#616161',
+      logo: '#FFFFFF',
+      main: '#FFFFFF',
+      logoTitle: '#FFFFFF',
+      text: '#2B2B2B'
+    },
+    yellow: {
+      bg: '#FFF4D7',
+      title: '#FFE7B7',
+      logo: 'transparent',
+      main: '#000000',
+      logoTitle: '#4A8731',
+      text: '#FFFFFF'
+    }
+  }
+  document.getElementById('bg').style.background = themeTypes[color]['bg'];  
+  document.getElementById('logo').style.background = themeTypes[color]['logo'];
+  document.getElementById('main').style.color = themeTypes[color]['main'];
+  document.getElementById('logoTitle').style.color = themeTypes[color]['logoTitle'];
+  document.getElementById('ljTitle').style.background = themeTypes[color]['title'];
+  document.getElementById('ljText').style.background = themeTypes[color]['text'];
+  document.getElementById('dgTitle').style.background = themeTypes[color]['title'];
+  document.getElementById('dgText').style.background = themeTypes[color]['text'];
+  document.getElementById('diffedTitle').style.background = themeTypes[color]['title'];
+  document.getElementById('diffedText').style.background = themeTypes[color]['text'];
+}
--- src/containers/themeSelector.js
+++ src/containers/themeSelector.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import {DropdownButton, MenuItem} from 'react-bootstrap';
+import {changeTheme} from './changeTheme.js';
 
 const render = () => {
   return (
     <div id="themeSelector">
       <DropdownButton title="Theme" id="themeDropdown" bsStyle={'Info'.toLowerCase()}>
-        <MenuItem id="themeGreen" eventKey="themeGreen">Green</MenuItem>
-        <MenuItem id="themeBlue" eventKey="themeBlue">Blue</MenuItem>
-        <MenuItem id="themeBlack" eventKey="themeBlack">Black</MenuItem>
-        <MenuItem id="themeYellow" eventKey="themeYellow">Yellow</MenuItem>
+        <MenuItem id="themeGreen" eventKey="themeGreen" onClick={() => {changeTheme('green')}}>Green</MenuItem>
+        <MenuItem id="themeBlue" eventKey="themeBlue" onClick={() => {changeTheme('blue')}}>Blue</MenuItem>
+        <MenuItem id="themeBlack" eventKey="themeBlack" onClick={() => {changeTheme('black')}}>Black</MenuItem>
+        <MenuItem id="themeYellow" eventKey="themeYellow" onClick={() => {changeTheme('yellow')}}>Yellow</MenuItem>
       </DropdownButton>
     </div>
   )
