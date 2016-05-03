22e86d6beb52701bb1774ec5b628c756b0a2a0ed
zhi-xuan <rickie120243@gmail.com>
2016-05-03 18:00:46 +0800

    add pane resize

--- src/components/showDiff.js
+++ src/components/showDiff.js
@@ -2,29 +2,38 @@ import React, {Component} from 'react';
 import LjText from '../containers/ljText.js';
 import DgText from '../containers/dgText.js';
 import DiffedText from '../containers/diffedText.js';
+import Resizable from 'react-resizable-box';
 
 export default class ShowDiff extends Component {
   render() {
     return (
       <div id="main">
-        <span id="ljRender">
-          <div id="ljTitle">LJ</div>
-          <div id="ljText">
+        <Resizable
+          width="33%"
+        >
+          <span id="ljRender">
+            <div id="ljTitle">LJ</div>
+            <div id="ljText">
             <LjText />
-          </div>
-        </span>
+            </div>
+          </span>
+        </Resizable>
         <span id="dgRender">
           <div id="dgTitle">DG</div>
           <div id="dgText">
-            <DgText />
+          <DgText />
           </div>
         </span>
-        <span id="diffedRender">
-          <div id="diffedTitle">Diffed</div>
-          <div id="diffedText">
+        <Resizable
+          width="33%"
+        >
+          <span id="diffedRender">
+            <div id="diffedTitle">Diffed</div>
+            <div id="diffedText">
             <DiffedText />
-          </div>
-        </span>
+            </div>
+          </span>
+        </Resizable>
       </div>
     )
   }
