34a1b4df17c0c707d8e01785df0cb94cd0bfd6eb
zhi-xuan <rickie120243@gmail.com>
2016-05-09 14:18:52 +0800

    fix resize style

--- src/components/Diff.js
+++ src/components/Diff.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import LjText from '../containers/LjText.js';
-import DgText from '../containers/DgText.js';
+import OldText from '../containers/OldText.js';
+import NewText from '../containers/NewText.js';
 import DiffedText from '../containers/DiffedText.js';
 import Resizable from 'react-resizable-box';
 
@@ -8,11 +8,11 @@ export default class ShowDiff extends Component {
   render() {
     return (
       <div id="main">
-        <Resizable width="33%" minWidth={90} isResizable={{top:false, right:true, bottom:false, left:false, topRight:false, bottomRight:false, bottomLeft:false, topLeft:false}}>
-          <LjText />
+        <Resizable width="33%" minWidth={90} height="100%" isResizable={{top:false, right:true, bottom:false, left:false, topRight:false, bottomRight:false, bottomLeft:false, topLeft:false}}>
+          <OldText />
         </Resizable>
-        <DgText />
-        <Resizable width="33%" minWidth={90} isResizable={{top:false, right:false, bottom:false, left:true, topRight:false, bottomRight:false, bottomLeft:false, topLeft:false}}>
+        <NewText />
+        <Resizable width="33%" minWidth={90} height="100%" isResizable={{top:false, right:false, bottom:false, left:true, topRight:false, bottomRight:false, bottomLeft:false, topLeft:false}}>
           <DiffedText />
         </Resizable>
       </div>
