30676e6ec8a210f5fc3fca91f1d8a89fb28c1a3a
zhi-xuan <rickie120243@gmail.com>
2016-05-19 10:46:58 +0800

    fix css & rm logo.png

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import 'babel-polyfill';
 import 'bootstrap-css';
 import './index.css';
-import './../assets/img/logo.png';
 import React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
