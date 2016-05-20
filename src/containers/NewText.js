e5dc3a3da946a6277107aa34e4d009d455197978
zhi-xuan <rickie120243@gmail.com>
2016-05-20 10:52:33 +0800

    multiple codemirror problem

--- src/containers/NewText.js
+++ src/containers/NewText.js
@@ -1,15 +1,9 @@
 require('codemirror/mode/javascript/javascript');
 require('codemirror/mode/xml/xml');
-require('codemirror/mode/markdown/markdown');
 
 import React from 'react';
 import {connect} from 'react-redux';
 import Codemirror from 'react-codemirror';
-import Resizable from 'react-resizable-box';
-
-const hideRender = () => {
-
-};
 
 let options = {
   mode: 'text/html',
@@ -24,7 +18,7 @@ const render = (state) => {
   return (
     <span id="newRender">
       <div id="newTitle">{name}{currentPage}<div className="closeRender">+</div></div>
-      <Codemirror id="newC" value={state.pager.newText} options={options} />
+      <Codemirror value={state.pager.newText} options={options} />
     </span>
   );
 };
--- src/containers/OldText.js
+++ src/containers/OldText.js
@@ -1,6 +1,5 @@
 require('codemirror/mode/javascript/javascript');
 require('codemirror/mode/xml/xml');
-require('codemirror/mode/markdown/markdown');
 
 import React from 'react';
 import {connect} from 'react-redux';
@@ -10,7 +9,8 @@ import Resizable from 'react-resizable-box';
 let options = {
   mode: 'text/html',
   lineNumbers: true,
-  lineWrapping: true
+  lineWrapping: true,
+  readOnly: true
 };
 
 const render = (state) => {
@@ -20,7 +20,7 @@ const render = (state) => {
     <Resizable width="33%" minWidth={100} isResizable={{top:false, right:true, bottom:false, left:false, topRight:false, bottomRight:false, bottomLeft:false, topLeft:false}}>
       <span id="oldRender">
         <div id="oldTitle">{name}{currentPage}<div className="closeRender">+</div></div>
-        <Codemirror id="oldC" value={state.pager.oldText} options={options} />
+        <Codemirror value={state.pager.oldText} options={options} />
       </span>
     </Resizable>
   );
