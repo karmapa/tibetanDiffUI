7091fc385aa3f2733af6d41483abba3e4a4afb0f
zhi-xuan <rickie120243@gmail.com>
2016-05-20 16:29:03 +0800

    add shouldComponentUpdate

--- src/containers/NewText.js
+++ src/containers/NewText.js
@@ -1,27 +1,42 @@
 require('codemirror/mode/javascript/javascript');
 require('codemirror/mode/xml/xml');
 
-import React from 'react';
+import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import Codemirror from 'react-codemirror';
+import Resizable from 'react-resizable-box';
 
-let options = {
-  mode: 'text/html',
-  lineNumbers: true,
-  lineWrapping: true,
-  readOnly: true
-};
+class NewTextContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      options: {
+        mode: 'text/html',
+        lineNumbers: true,
+        lineWrapping: true,
+        matchBrackets: true,
+        readOnly: true
+      }
+    };
+  }
 
-const render = (state) => {
-  let name = state.pager.newTextName;
-  let currentPage = '-' + state.pager.currentPage;
-  return (
-    <span id="newRender">
-      <div id="newTitle">{name}{currentPage}<div className="closeRender">+</div></div>
-      <Codemirror value={state.pager.newText} options={options} />
-    </span>
-  );
-};
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.pager.currentPage !== this.props.pager.currentPage;
+  }
+
+  render() {
+    let name = this.props.pager.newTextName;
+    let currentPage = '-' + this.props.pager.currentPage;
+    return (
+      <Resizable width="33%" minWidth={100} isResizable={{top:false, right:true, bottom:false, left:false, topRight:false, bottomRight:false, bottomLeft:false, topLeft:false}}>
+        <span id="oldRender">
+          <div id="oldTitle">{name}{currentPage}<div className="closeRender">+</div></div>
+          <Codemirror value={this.props.pager.newText} options={this.state.options} />
+        </span>
+      </Resizable>
+    );
+  }
+}
 
 const mapStateToProps = (state) => {
   return {
@@ -29,6 +44,6 @@ const mapStateToProps = (state) => {
   };
 };
 
-const NewText = connect(mapStateToProps)(render);
+const NewText = connect(mapStateToProps)(NewTextContainer);
 
 export default NewText;
--- src/containers/OldText.js
+++ src/containers/OldText.js
@@ -1,30 +1,42 @@
 require('codemirror/mode/javascript/javascript');
 require('codemirror/mode/xml/xml');
 
-import React from 'react';
+import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import Codemirror from 'react-codemirror';
 import Resizable from 'react-resizable-box';
 
-let options = {
-  mode: 'text/html',
-  lineNumbers: true,
-  lineWrapping: true,
-  readOnly: true
-};
+class OldTextContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      options: {
+        mode: 'text/html',
+        lineNumbers: true,
+        lineWrapping: true,
+        matchBrackets: true,
+        readOnly: true
+      }
+    };
+  }
 
-const render = (state) => {
-  let name = state.pager.oldTextName;
-  let currentPage = '-' + state.pager.currentPage;
-  return (
-    <Resizable width="33%" minWidth={100} isResizable={{top:false, right:true, bottom:false, left:false, topRight:false, bottomRight:false, bottomLeft:false, topLeft:false}}>
-      <span id="oldRender">
-        <div id="oldTitle">{name}{currentPage}<div className="closeRender">+</div></div>
-        <Codemirror value={state.pager.oldText} options={options} />
-      </span>
-    </Resizable>
-  );
-};
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.pager.currentPage !== this.props.pager.currentPage;
+  }
+
+  render() {
+    let name = this.props.pager.oldTextName;
+    let currentPage = '-' + this.props.pager.currentPage;
+    return (
+      <Resizable width="33%" minWidth={100} isResizable={{top:false, right:true, bottom:false, left:false, topRight:false, bottomRight:false, bottomLeft:false, topLeft:false}}>
+        <span id="oldRender">
+          <div id="oldTitle">{name}{currentPage}<div className="closeRender">+</div></div>
+          <Codemirror value={this.props.pager.oldText} options={this.state.options} />
+        </span>
+      </Resizable>
+    );
+  }
+}
 
 const mapStateToProps = (state) => {
   return {
@@ -32,6 +44,6 @@ const mapStateToProps = (state) => {
   };
 };
 
-const OldText = connect(mapStateToProps)(render);
+const OldText = connect(mapStateToProps)(OldTextContainer);
 
 export default OldText;
