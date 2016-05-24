c4164811d1a37877d04663dbeb6bd4e92d65040f
zhi-xuan <rickie120243@gmail.com>
2016-05-24 17:29:36 +0800

    split functin work

--- src/containers/DiffedText.js
+++ src/containers/DiffedText.js
@@ -16,32 +16,34 @@ const diff = (oldText, newText) => {
   });
 };
 
+const drawColor = (arr, idx) => {
+  let diffedDrawBlack = 'diffedDrawBlack';
+  let color = diffedDrawBlack;
+  if ('black' !== arr[0] && (arr[1].match(/[\u0f0b-\u0f12]/) || arr[1].match(/^[ ]+$/))) {
+    return <span key={idx} id="diffedDrawBlack">{arr[1]}</span>;
+  }
+  if ('green' === arr[0]) {
+    color = 'diffedDrawGreen';
+  } else if ('red' === arr[0]) {
+    color = 'diffedDrawRed';
+  }
+  if (arr[1].match(/^[\r\n]+$/)) {
+    return <div key={idx + 50}></div>;
+  } else if (arr[1].match(/^[\r\n]+/)) {
+    arr[1] = arr[1].replace(/[\r\n]/, '');
+    return <span key={idx + 100}><div key={idx + 50}></div><span key={idx} id={color} >{arr[1]}</span></span>;
+  } else if (arr[1].match(/[\r\n]+$/)) {
+    arr[1] = arr[1].replace(/[\r\n]/, '');
+    return <span key={idx + 100}><span key={idx} id={color} >{arr[1]}</span><div key={idx + 50}></div></span>;
+  } else {
+    return <span key={idx + 100}><span key={idx} id={color} >{arr[1]}</span></span>;
+  }
+};
+
 const render = (state) => {
   let newTextNoLinebreak = state.pager.newText.replace(/\r\n/g, '');
   let arr = diff(state.pager.oldText, newTextNoLinebreak);
-  let output = arr.map((word, idx) => {
-    if ('black' !== word[0] && (word[1].match(/[\u0f0b-\u0f12]/) || word[1].match(/^[ ]+$/))) {
-      return <span key={idx} id="diffedDrawBlack">{word[1]}</span>;
-    }
-    let diffedDrawBlack = 'diffedDrawBlack';
-    let color = diffedDrawBlack;
-    if ('green' === word[0]) {
-      color = 'diffedDrawGreen';
-    } else if ('red' === word[0]) {
-      color = 'diffedDrawRed';
-    }
-    if (word[1].match(/^[\r\n]+$/)) {
-      return <div key={idx + 50}></div>;
-    } else if (word[1].match(/^[\r\n]+/)) {
-      word[1] = word[1].replace(/[\r\n]/, '');
-      return <span key={idx + 100}><div key={idx + 50}></div><span key={idx} id={color} >{word[1]}</span></span>;
-    } else if (word[1].match(/[\r\n]+$/)) {
-      word[1] = word[1].replace(/[\r\n]/, '');
-      return <span key={idx + 100}><span key={idx} id={color} >{word[1]}</span><div key={idx + 50}></div></span>;
-    } else {
-      return <span key={idx + 100}><span key={idx} id={color} >{word[1]}</span></span>;
-    }
-  });
+  let output = arr.map(drawColor);
   return (
     <Resizable width="33%" minWidth={100} height="100%" isResizable={{top:false, right:false, bottom:false, left:true, topRight:false, bottomRight:false, bottomLeft:false, topLeft:false}}>
       <span id="diffedRender">
